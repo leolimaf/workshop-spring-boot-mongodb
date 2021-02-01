71c06d4b8ecc9128dbd1847f3723c5d80f1e33a6
Leonardo Lima <leolimaf@outlook.com>
2021-02-01 10:33:52 -0300

    Project created

---
+++ src/test/java/dev/leolimaf/workshopspringbootmongodb/WorkshopSpringBootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.leolimaf.workshopspringbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringBootMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
