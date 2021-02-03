f5484ad4178a018503c3edca96aa55b3244e99ec
Leonardo Lima <leolimaf@outlook.com>
2021-02-03 11:57:08 -0300

    Adding comments to posts

--- src/main/java/dev/leolimaf/workshopspringbootmongodb/config/Instantiation.java
+++ src/main/java/dev/leolimaf/workshopspringbootmongodb/config/Instantiation.java
@@ -3,6 +3,7 @@ package dev.leolimaf.workshopspringbootmongodb.config;
 import dev.leolimaf.workshopspringbootmongodb.domain.Post;
 import dev.leolimaf.workshopspringbootmongodb.domain.User;
 import dev.leolimaf.workshopspringbootmongodb.dto.AuthorDTO;
+import dev.leolimaf.workshopspringbootmongodb.dto.CommentDTO;
 import dev.leolimaf.workshopspringbootmongodb.repositories.PostRepository;
 import dev.leolimaf.workshopspringbootmongodb.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.TimeZone;
 
 @Configuration
@@ -40,6 +42,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"),"Let's go!", "I'm gonna travel to São Paulo", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Good morning!", "Hey guys! Good morning!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Have a nice trip!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Enjoy it!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Have a good one!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1,c2));
+        post2.getComments().addAll(Collections.singletonList(c3));
 
         postRepository.saveAll(Arrays.asList(post1,post2));
 
--- src/main/java/dev/leolimaf/workshopspringbootmongodb/domain/Post.java
+++ src/main/java/dev/leolimaf/workshopspringbootmongodb/domain/Post.java
@@ -1,11 +1,14 @@
 package dev.leolimaf.workshopspringbootmongodb.domain;
 
 import dev.leolimaf.workshopspringbootmongodb.dto.AuthorDTO;
+import dev.leolimaf.workshopspringbootmongodb.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -19,6 +22,8 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO authorDTO;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {
     }
@@ -69,6 +74,14 @@ public class Post implements Serializable {
 
     public void setAuthorDTO(AuthorDTO authorDTO) {
         this.authorDTO = authorDTO;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
